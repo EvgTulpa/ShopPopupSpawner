c9dfea9377d4bc0342428f670aa3945fd2b1e332
EvgTulpa <103635242+EvgTulpa@users.noreply.github.com>
2022-05-25 15:55:20 +0300

    Update ShopPopupSpawnerViewComponent.cs

--- ShopPopupSpawnerViewComponent.cs
+++ ShopPopupSpawnerViewComponent.cs
@@ -6,6 +6,16 @@ namespace Murka
     {
         [SerializeField] private ShopPopupSpawnerData ShopPopupSpawnerData;
 
+        public void SetReason(string value)
+        {
+            ShopPopupSpawnerData.Reason = value;
+        }
+        
+        public void SetPage(int value)
+        {
+            ShopPopupSpawnerData.Page = value;
+        }
+
         public void OpenShopPopup()
         {
             ShopPopupSpawnerUtil.OpenShopPopup(ShopPopupSpawnerData);
