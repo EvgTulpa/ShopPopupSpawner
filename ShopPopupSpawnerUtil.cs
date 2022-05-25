e69c6505776125f170e874df0f110f61e3fb5603
EvgTulpa <103635242+EvgTulpa@users.noreply.github.com>
2022-05-25 17:28:43 +0300

    Update ShopPopupSpawnerUtil.cs

--- ShopPopupSpawnerUtil.cs
+++ ShopPopupSpawnerUtil.cs
@@ -27,16 +27,17 @@ namespace Murka
         
         public static void OpenShopPopup(ShopPopupSpawnerData data)
         {
-            Dictionary<string, object> payload = BuyPopupHelper.AddBuyPopup(data.Reason, data.CoinsPage ? 1 : 2, 0);
+            bool isCoinPage = data.CoinsPage;
+            Dictionary<string, object> payload = BuyPopupHelper.AddBuyPopup(data.Reason, isCoinPage ? 1 : 2, 0);
             OpenSlotTracker.AddReason(payload);
             AdditionalTrackersParams.AddSaleGrope("buy_menu",payload);
             AdditionalTrackersParams.AddSaleMechanics("buy_menu",payload);
             
             string promoType = OfferManager.Instance.GetActiveBuyMenuReskin() ?? "base";
             
-            string moneyType = data.CoinsPage ? "coins" : "gems";
-            string moneyTrackerType = data.CoinsPage ? StoreTrackers.STORE_OPEN_COINS : StoreTrackers.STORE_OPEN_GEMS;
-            if (data.CoinsPage)
+            string moneyType = isCoinPage ? "coins" : "gems";
+            string moneyTrackerType = isCoinPage ? StoreTrackers.STORE_OPEN_COINS : StoreTrackers.STORE_OPEN_GEMS;
+            if (isCoinPage)
             {
                 BillsTrackers.Instance.AddReason(payload);
             }
