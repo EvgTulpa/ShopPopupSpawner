c30e25f757bae4ed4222e58ce3d325bcef8eb917
EvgTulpa <103635242+EvgTulpa@users.noreply.github.com>
2022-05-25 17:42:00 +0300

    Update ShopPopupSpawnerUtil.cs

--- ShopPopupSpawnerUtil.cs
+++ ShopPopupSpawnerUtil.cs
@@ -44,5 +44,25 @@ namespace Murka
             AdditionalTrackersParams.AddPriceList<DefaultPriceListWithSubscriptionGenerator, List<PriceVO>>(moneyType, promoType, payload);
             MurkaCore.Instance.Tracker.track(new TrackerVO(moneyTrackerType, string.Empty, payload));
         }
+        
+        public static void OpenShopPopup(ShopPopupSpawnerData data)
+        {
+            bool isCoinPage = data.IsCoinPage();
+            
+            Dictionary<string, object> payload = BuyPopupHelper.AddBuyPopup(data.Reason, isCoinPage ? 1 : 2, 0);
+            OpenSlotTracker.AddReason(payload);
+            AdditionalTrackersParams.AddSaleGrope("buy_menu",payload);
+            AdditionalTrackersParams.AddSaleMechanics("buy_menu",payload);
+            
+            string promoType = OfferManager.Instance.GetActiveBuyMenuReskin() ?? "base";
+            string moneyTrackerType = isCoinPage ? StoreTrackers.STORE_OPEN_COINS : StoreTrackers.STORE_OPEN_GEMS;
+            if (isCoinPage)
+            {
+                BillsTrackers.Instance.AddReason(payload);
+            }
+
+            AdditionalTrackersParams.AddPriceList<DefaultPriceListWithSubscriptionGenerator, List<PriceVO>>(data.PageType, promoType, payload);
+            MurkaCore.Instance.Tracker.track(new TrackerVO(moneyTrackerType, string.Empty, payload));
+        }
     }
 }
