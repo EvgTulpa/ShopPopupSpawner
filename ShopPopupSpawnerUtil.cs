d2c721e1c690aa6791fe373b6017a244b0a05f91
EvgTulpa <103635242+EvgTulpa@users.noreply.github.com>
2022-05-26 12:32:21 +0300

    Update ShopPopupSpawnerUtil.cs

--- ShopPopupSpawnerUtil.cs
+++ ShopPopupSpawnerUtil.cs
@@ -7,28 +7,8 @@ namespace Murka
     {
         public static void OpenShopPopup(ShopPopupSpawnerData data)
         {
-            Dictionary<string, object> payload = BuyPopupHelper.AddBuyPopup(data.Reason, data.Page, 0);
-            OpenSlotTracker.AddReason(payload);
-            AdditionalTrackersParams.AddSaleGrope("buy_menu",payload);
-            AdditionalTrackersParams.AddSaleMechanics("buy_menu",payload);
-            
-            string promoType = OfferManager.Instance.GetActiveBuyMenuReskin() ?? "base";
-            bool isCoinPage = page == 1;
-            string moneyType = isCoinPage ? "coins" : "gems";
-            string moneyTrackerType = isCoinPage ? StoreTrackers.STORE_OPEN_COINS : StoreTrackers.STORE_OPEN_GEMS;
-            if (isCoinPage)
-            {
-                BillsTrackers.Instance.AddReason(payload);
-            }
-
-            AdditionalTrackersParams.AddPriceList<DefaultPriceListWithSubscriptionGenerator, List<PriceVO>>(moneyType, promoType, payload);
-            MurkaCore.Instance.Tracker.track(new TrackerVO(moneyTrackerType, string.Empty, payload));
-        }
-        
-        //v2
-        public static void OpenShopPopup(ShopPopupSpawnerData data)
-        {
             bool isCoinPage = data.CoinsPage;
+            
             Dictionary<string, object> payload = BuyPopupHelper.AddBuyPopup(data.Reason, isCoinPage ? 1 : 2, 0);
             OpenSlotTracker.AddReason(payload);
             AdditionalTrackersParams.AddSaleGrope("buy_menu",payload);
@@ -43,27 +23,6 @@ namespace Murka
             }
 
             AdditionalTrackersParams.AddPriceList<DefaultPriceListWithSubscriptionGenerator, List<PriceVO>>(moneyType, promoType, payload);
-            MurkaCore.Instance.Tracker.track(new TrackerVO(moneyTrackerType, string.Empty, payload));
-        }
-        
-        // v3
-        public static void OpenShopPopup(ShopPopupSpawnerData data)
-        {
-            bool isCoinPage = data.IsCoinPage();
-            
-            Dictionary<string, object> payload = BuyPopupHelper.AddBuyPopup(data.Reason, isCoinPage ? 1 : 2, 0);
-            OpenSlotTracker.AddReason(payload);
-            AdditionalTrackersParams.AddSaleGrope("buy_menu",payload);
-            AdditionalTrackersParams.AddSaleMechanics("buy_menu",payload);
-            
-            string promoType = OfferManager.Instance.GetActiveBuyMenuReskin() ?? "base";
-            string moneyTrackerType = isCoinPage ? StoreTrackers.STORE_OPEN_COINS : StoreTrackers.STORE_OPEN_GEMS;
-            if (isCoinPage)
-            {
-                BillsTrackers.Instance.AddReason(payload);
-            }
-
-            AdditionalTrackersParams.AddPriceList<DefaultPriceListWithSubscriptionGenerator, List<PriceVO>>(data.PageType, promoType, payload);
             MurkaCore.Instance.Tracker.track(new TrackerVO(moneyTrackerType, string.Empty, payload));
         }
     }
